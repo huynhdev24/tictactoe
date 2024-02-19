a9be13775f17cd199556d4d9ceca38322384bf3e
huynhdev24 <huynh.dev.24@gmail.com>
2024-02-19 13:38:54 +0700

    fix: change values of Squares (#4)

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ export default function Board() {
     <>
       <div className="board-row">
         <Square value="1"></Square>
-        <Square value="1"></Square>
-        <Square value="1"></Square>
+        <Square value="2"></Square>
+        <Square value="3"></Square>
       </div>
       <div className="board-row">
-        <Square value="1"></Square>
-        <Square value="1"></Square>
-        <Square value="1"></Square>
+        <Square value="4"></Square>
+        <Square value="5"></Square>
+        <Square value="6"></Square>
       </div>
       <div className="board-row">
-        <Square value="1"></Square>
-        <Square value="1"></Square>
-        <Square value="1"></Square>
+        <Square value="7"></Square>
+        <Square value="8"></Square>
+        <Square value="9"></Square>
       </div>
     </>
   )
